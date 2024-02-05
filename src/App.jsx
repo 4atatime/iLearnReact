b058406fb12960d4475f21b35a51bcb1b29e5559
Jo Vinkenroye <jo.vinkenroye@mow.vlaanderen.be>
2024-02-05 21:24:49 +0100

    oranges

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import './App.css'
-import Chicken from './Chicken'
-import Greeter from './Greeter'
-import Die from './Die'
-import ListPicker from './ListPicker'
-import DoubleDie from './DoubleDie'
-import Heading from './Heading'
-import ColorList from './ColorList'
-import ShopList from './ShopList'
-import ShopListItem from './ShopListItem'
+import "./App.css";
+import Chicken from "./Chicken";
+import Greeter from "./Greeter";
+import Die from "./Die";
+import ListPicker from "./ListPicker";
+import DoubleDie from "./DoubleDie";
+import Heading from "./Heading";
+import ColorList from "./ColorList";
+import ShopList from "./ShopList";
+import ShopListItem from "./ShopListItem";
 
-const data=[
-  {id:1, name:"eggs", quantity: 12, status: false},
-  {id:2, name:"milk", quantity: 7, status: true},
-  {id:3, name:"cookies", quantity: 4, status: false},
-  {id:4, name:"apples", quantity: 10, status: true},
-]
+const data = [
+ { id: 1, name: "eggs", quantity: 12, status: false },
+ { id: 2, name: "milk", quantity: 7, status: true },
+ { id: 3, name: "cookies", quantity: 4, status: false },
+ { id: 4, name: "apples", quantity: 10, status: true },
+ { id: 5, name: "oranges", quantity: 20, status: false },
+];
 
 function App() {
-  return (
-    <div>
-      <ShopList items = {data}/>
-      {/* <ColorList colors={["red", "blue","yellow"]} fontSize="24px"/> */}
-      {/* <Heading color="red" text="heyho!" fontSize="20px"/> */}
-      {/* <DoubleDie />
+ return (
+  <div>
+   <ShopList items={data} />
+   {/* <ColorList colors={["red", "blue","yellow"]} fontSize="24px"/> */}
+   {/* <Heading color="red" text="heyho!" fontSize="20px"/> */}
+   {/* <DoubleDie />
       <DoubleDie />
       <ListPicker values = {[1, 2, 3]}/>
       <ListPicker values = {["a", "b", "c"]}/>
@@ -30,8 +31,8 @@ function App() {
       <Greeter person="Aya"/>
       <Die numSides={20}/>
       <Die numSides={7}/>  */}
-    </div>
-  );
+  </div>
+ );
 }
 
-export default App
+export default App;
